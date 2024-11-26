aaab169ce7fa4e0990c66921a8f4c6ef81d8ebd9
Enzo Azevedo <enzo.rva@gmail.com>
2024-11-25 23:42:08 -0300

    convert to OOP

--- bot.py
+++ bot.py
@@ -1,120 +1,124 @@
 import random
-o = "o"
-x = "x"
 
-def bot(display_grid,board, j):
-    if j == 1:
-        choice = random.randrange(9)
-        quadrant = int(choice) - 1
-        while board[quadrant] != " ":
+class Bot:
+    def __init__(self):
+        self.o = "o"
+        self.x = "x"
+
+
+    def bot(self, display_grid,board, j):
+        if j == 1:
             choice = random.randrange(9)
             quadrant = int(choice) - 1
-        board[quadrant] = o
-        display_grid()
-    else:
-    # Winning Moves
-        for i in range(0, 9, 3):  # Check rows
-            if board[i] == board[i + 1] == o and board[i + 2] == " ":
-                board[i + 2] = o
-                display_grid()
-                return
-            elif board[i + 1] == board[i + 2] == o and board[i] == " ":
-                board[i] = o
-                display_grid()
-                return
-            elif board[i] == board[i + 2] == o and board[i + 1] == " ":
-                board[i + 1] = o
-                display_grid()
-                return
-
-        for i in range(3):  # Check columns
-            if board[i] == board[i + 3] == o and board[i + 6] == " ":
-                board[i + 6] = o
-                display_grid()
-                return
-            elif board[i + 3] == board[i + 6] == o and board[i] == " ":
-                board[i] = o
-                display_grid()
-                return
-            elif board[i] == board[i + 6] == o and board[i + 3] == " ":
-                board[i + 3] = o
-                display_grid()
-                return
-
-        # Check diagonals
-        diagonals = [(0, 4, 8), (2, 4, 6)]
-        for d in diagonals:
-            if board[d[0]] == board[d[1]] == o and board[d[2]] == " ":
-                board[d[2]] = o
-                display_grid()
-                return
-            elif board[d[1]] == board[d[2]] == o and board[d[0]] == " ":
-                board[d[0]] = o
-                display_grid()
-                return
-            elif board[d[0]] == board[d[2]] == o and board[d[1]] == " ":
-                board[d[1]] = o
-                display_grid()
-                return
-
-        # Blocking Moves
-        for i in range(0, 9, 3):  # Check rows
-            if board[i] == board[i + 1] == x and board[i + 2] == " ":
-                board[i + 2] = o
-                display_grid()
-                return
-            elif board[i + 1] == board[i + 2] == x and board[i] == " ":
-                board[i] = o
-                display_grid()
-                return
-            elif board[i] == board[i + 2] == x and board[i + 1] == " ":
-                board[i + 1] = o
-                display_grid()
-                return
-
-        for i in range(3):  # Check columns
-            if board[i] == board[i + 3] == x and board[i + 6] == " ":
-                board[i + 6] = o
-                display_grid()
-                return
-            elif board[i + 3] == board[i + 6] == x and board[i] == " ":
-                board[i] = o
-                display_grid()
-                return
-            elif board[i] == board[i + 6] == x and board[i + 3] == " ":
-                board[i + 3] = o
-                display_grid()
-                return
-
-        # Check diagonals
-        for d in diagonals:
-            if board[d[0]] == board[d[1]] == x and board[d[2]] == " ":
-                board[d[2]] = o
-                display_grid()
-                return
-            elif board[d[1]] == board[d[2]] == x and board[d[0]] == " ":
-                board[d[0]] = o
-                display_grid()
-                return
-            elif board[d[0]] == board[d[2]] == x and board[d[1]] == " ":
-                board[d[1]] = o
-                display_grid()
-                return
-
-        # Strategic Moves (fallback)
-        if board[4] == " ":  # Center
-            board[4] = o
+            while board[quadrant] != " ":
+                choice = random.randrange(9)
+                quadrant = int(choice) - 1
+            board[quadrant] = self.o
             display_grid()
-            return
+        else:
+        # Winning Moves
+            for i in range(0, 9, 3):  # Check rows
+                if board[i] == board[i + 1] == self.o and board[i + 2] == " ":
+                    board[i + 2] = self.o
+                    display_grid()
+                    return
+                elif board[i + 1] == board[i + 2] == self.o and board[i] == " ":
+                    board[i] = self.o
+                    display_grid()
+                    return
+                elif board[i] == board[i + 2] == self.o and board[i + 1] == " ":
+                    board[i + 1] = self.o
+                    display_grid()
+                    return
 
-        for corner in [0, 2, 6, 8]:  # Corners
-            if board[corner] == " ":
-                board[corner] = o
+            for i in range(3):  # Check columns
+                if board[i] == board[i + 3] == self.o and board[i + 6] == " ":
+                    board[i + 6] = self.o
+                    display_grid()
+                    return
+                elif board[i + 3] == board[i + 6] == self.o and board[i] == " ":
+                    board[i] = self.o
+                    display_grid()
+                    return
+                elif board[i] == board[i + 6] == self.o and board[i + 3] == " ":
+                    board[i + 3] = self.o
+                    display_grid()
+                    return
+
+            # Check diagonals
+            diagonals = [(0, 4, 8), (2, 4, 6)]
+            for d in diagonals:
+                if board[d[0]] == board[d[1]] == self.o and board[d[2]] == " ":
+                    board[d[2]] = self.o
+                    display_grid()
+                    return
+                elif board[d[1]] == board[d[2]] == self.o and board[d[0]] == " ":
+                    board[d[0]] = self.o
+                    display_grid()
+                    return
+                elif board[d[0]] == board[d[2]] == self.o and board[d[1]] == " ":
+                    board[d[1]] = self.o
+                    display_grid()
+                    return
+
+            # Blocking Moves
+            for i in range(0, 9, 3):  # Check rows
+                if board[i] == board[i + 1] == self.x and board[i + 2] == " ":
+                    board[i + 2] = self.o
+                    display_grid()
+                    return
+                elif board[i + 1] == board[i + 2] == self.x and board[i] == " ":
+                    board[i] = self.o
+                    display_grid()
+                    return
+                elif board[i] == board[i + 2] == self.x and board[i + 1] == " ":
+                    board[i + 1] = self.o
+                    display_grid()
+                    return
+
+            for i in range(3):  # Check columns
+                if board[i] == board[i + 3] == self.x and board[i + 6] == " ":
+                    board[i + 6] = self.o
+                    display_grid()
+                    return
+                elif board[i + 3] == board[i + 6] == self.x and board[i] == " ":
+                    board[i] = self.o
+                    display_grid()
+                    return
+                elif board[i] == board[i + 6] == self.x and board[i + 3] == " ":
+                    board[i + 3] = self.o
+                    display_grid()
+                    return
+
+            # Check diagonals
+            for d in diagonals:
+                if board[d[0]] == board[d[1]] == self.x and board[d[2]] == " ":
+                    board[d[2]] = self.o
+                    display_grid()
+                    return
+                elif board[d[1]] == board[d[2]] == self.x and board[d[0]] == " ":
+                    board[d[0]] = self.o
+                    display_grid()
+                    return
+                elif board[d[0]] == board[d[2]] == self.x and board[d[1]] == " ":
+                    board[d[1]] = self.o
+                    display_grid()
+                    return
+
+            # Strategic Moves (fallback)
+            if board[4] == " ":  # Center
+                board[4] = self.o
                 display_grid()
                 return
 
-        for edge in [1, 3, 5, 7]:  # Edges
-            if board[edge] == " ":
-                board[edge] = o
-                display_grid()
-                return
+            for corner in [0, 2, 6, 8]:  # Corners
+                if board[corner] == " ":
+                    board[corner] = self.o
+                    display_grid()
+                    return
+
+            for edge in [1, 3, 5, 7]:  # Edges
+                if board[edge] == " ":
+                    board[edge] = self.o
+                    display_grid()
+                    return
--- tic_tac_toe.py
+++ tic_tac_toe.py
@@ -1,26 +1,77 @@
-from bot import bot
+from bot import Bot
 
-board = [" "] * 9
-x = "x"
-o = "o"
+class TicTacToe:
+    def __init__(self):
+        self.board = [" "] * 9
+        self.x = "x"
+        self.o = "o"
+
+    def display_grid(self):
+        print(f"""
+            1    |2    |3
+              {self.board[0]}  |  {self.board[1]}  |  {self.board[2]} 
+            _____|_____|_____
+            4    |5    |6
+              {self.board[3]}  |  {self.board[4]}  |  {self.board[5]} 
+            _____|_____|_____
+            7    |8    |9
+              {self.board[6]}  |  {self.board[7]}  |  {self.board[8]}
+                 |     |
+            """)
+
+    def highlight_win(self, a, b, c):
+        if b == a + 1:
+            self.board[a], self.board[b], self.board[c] = "-", "-", "-"
+        elif b == a + 3:
+            self.board[a], self.board[b], self.board[c] = "|", "|", "|"
+        elif a == 0 and c == 8:
+            self.board[a], self.board[b], self.board[c] = "\\", "\\", "\\"
+        elif a == 2 and c == 6:
+            self.board[a], self.board[b], self.board[c] = "/", "/", "/"
 
 
-def display_grid():
-    print(f"""
-        1    |2    |3
-          {board[0]}  |  {board[1]}  |  {board[2]} 
-        _____|_____|_____
-        4    |5    |6
-          {board[3]}  |  {board[4]}  |  {board[5]} 
-        _____|_____|_____
-        7    |8    |9
-          {board[6]}  |  {board[7]}  |  {board[8]}
-             |     |
-        """)
+
+    def check_win_row(self, player):
+        for i in range(0, 9, 3):
+            if self.board[i] == self.board[i+1] == self.board[i+2] == player:
+                self.highlight_win(i, i+1, i+2)
+                return True
+                
+        return False  
+
+
+    def check_win_column(self, player):
+        columns = [[0, 3, 6], [1, 4, 7], [2, 5, 8]]
+        for col in columns:
+            if self.board[col[0]] == self.board[col[1]] == self.board[col[2]] == player:
+                self.highlight_win(col[0], col[1], col[2])
+                return True 
+        return False    
+
+
+    def check_win_diagonals(self, player):
+        diagonals = [[0, 4, 8], [2, 4, 6]]
+        for dialgonal in diagonals:
+            if self.board[dialgonal[0]] == self.board[dialgonal[1]] == self.board[dialgonal[2]] == player:
+                self.highlight_win(dialgonal[0], dialgonal[1], dialgonal[2])
+                return True
+        return False
 
 
 
-def get_play(player):
+    def check_win(self, player):
+        if self.check_win_row(player) or self.check_win_column(player) or self.check_win_diagonals(player):
+            print(f"Jogador {player} ganhou!!!")
+            return True
+        return False
+
+        
+class GameController:
+    def __init__(self):
+        self.game = TicTacToe()
+        self.adversary = Bot()
+
+    def get_play(self, player):
         while True:
             try:
                 play = input(f"Vez jogador {player}: ")
@@ -31,126 +82,56 @@ def get_play(player):
             except ValueError as e:
                 print(e)
 
-
-
-def highlight_win(a, b, c):
-    if b == a + 1:
-        board[a], board[b], board[c] = "-", "-", "-"
-    elif b == a + 3:
-        board[a], board[b], board[c] = "|", "|", "|"
-    elif a == 0 and c == 8:
-        board[a], board[b], board[c] = "\\", "\\", "\\"
-    elif a == 2 and c == 6:
-        board[a], board[b], board[c] = "/", "/", "/"
-
-
-
-def check_win_row(player):
-    for i in range(0, 9, 3):
-        if board[i] == board[i+1] == board[i+2] == player:
-            global winner
-            winner = player
-            highlight_win(i, i+1, i+2)
-            return True
-            
-    return False  
-
-
-def check_win_column(player):
-
-    columns = [[0, 3, 6], [1, 4, 7], [2, 5, 8]]
-
-    for col in columns:
-        if board[col[0]] == board[col[1]] == board[col[2]] == player:
-            global winner
-            winner = player
-            highlight_win(col[0], col[1], col[2])
-            return True 
-            
-    return False    
-
-
-def check_win_diagonals(player):
-
-    diagonals = [[0, 4, 8], [2, 4, 6]]
-
-    for dialgonal in diagonals:
-        if board[dialgonal[0]] == board[dialgonal[1]] == board[dialgonal[2]] == player:
-            global winner
-            winner = player
-            highlight_win(dialgonal[0], dialgonal[1], dialgonal[2])
-            return True
-    return False
-
-
-
-def check_win(player):
-    if check_win_row(player) or check_win_column(player) or check_win_diagonals(player):
-        print(f"Jogador {winner} ganhou!!!")
-        return True
-    return False
-
-        
-
-def game_loop(game_mode):
-    
-    
-    for j in range(9):
-        if game_mode == "bot":
-            if j % 2 == 0:
-                player = x
-                move = get_play(player)
+    def game_loop(self, game_mode):
+        for j in range(9):
+            if game_mode == "bot":
+                if j % 2 == 0:
+                    player = self.game.x
+                    move = self.get_play(player)
+                    quadrant = int(move) - 1 
+                    if self.game.board[quadrant] == " ":
+                        self.game.board[quadrant] = player
+                        self.game.display_grid()
+                        if self.game.check_win(player):
+                            self.game.display_grid()
+                            break        
+                    else:
+                        print("Quadrante Inválido")
+                    if j == 8:
+                        print("Deu véia!!!")
+                else:
+                    print("Vez jogador o...")
+                    self.adversary.bot(self.game.display_grid, self.game.board, j)
+                    player = self.game.o
+                    if self.game.check_win(player):
+                        self.game.display_grid()
+                        break
+                    if j == 8:
+                        print("Deu véia!!!")
+            elif game_mode == "player2":
+                player = self.game.x if j % 2 == 0 else self.game.o
+                move = self.get_play(player)
                 quadrant = int(move) - 1
-                
-                if board[quadrant] == " ":
-                    board[quadrant] = player
-                    display_grid()
-
-                    if check_win(player):
-                        display_grid()
-                        break        
+                if self.game.board[quadrant] == " ":
+                    self.game.board[quadrant] = player
+                    self.game.display_grid()
+                    if self.game.check_win(player):
+                        self.game.display_grid()
+                        break     
                 else:
                     print("Quadrante Inválido")
                 if j == 8:
                     print("Deu véia!!!")
             else:
-                print("Vez jogador o...")
-                bot(display_grid, board, j)
-                player = o
-                if check_win(player):
-                    display_grid()
-                    break
-                if j == 8:
-                    print("Deu véia!!!")
-
-        elif game_mode == "player2":
-            player = x if j % 2 == 0 else o
-            move = get_play(player)
-            quadrant = int(move) - 1
-            
-            if board[quadrant] == " ":
-                board[quadrant] = player
-                display_grid()
-
-                if check_win(player):
-                    display_grid()
-                    break
-                    
-                    
-            else:
-                print("Quadrante Inválido")
-
-            if j == 8:
-                print("Deu véia!!!")
-        else:
-            raise ValueError("Comando invalido, digite bot ou player2")
+                raise ValueError("Comando invalido, digite bot ou player2")
 
 
 def main():
+    controller = GameController()
     game_mode = input("Escolha modo de jogo (bot ou player2): ")
     while game_mode != "bot" and game_mode != "player2":
         game_mode = input("Escolha modo de jogo (bot ou player2): ")
-    display_grid()
-    game_loop(game_mode)
+    controller.game.display_grid()
+    controller.game_loop(game_mode)
 
 main()
